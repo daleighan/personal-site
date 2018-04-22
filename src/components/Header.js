d6408a9e51b4f1e991d287d5184e26c9405e7f05
David Leigh <daleighan@gmail.com>
2018-04-21 18:35:31 -0700

    Added tab browsing to the nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {Nav, NavItem} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
 
 const Header = () => (
-  <div>
-    <Link to='/about'>About</Link>
-    <Link to='/projects'>Projects</Link>
-    <Link to='/contact'>Contact</Link>
-  </div>
+  <Nav bsStyle="tabs" activeKey="1">
+    <LinkContainer exact to="/">
+      <NavItem eventKey={1}>Home</NavItem>
+    </LinkContainer>
+    <LinkContainer to="/about">
+      <NavItem eventKey={2}>About</NavItem>
+    </LinkContainer>
+    <LinkContainer to="/projects">
+      <NavItem eventKey={3}>Projects</NavItem>
+    </LinkContainer>
+    <LinkContainer to="/contact">
+      <NavItem eventKey={4}>Contact</NavItem>
+    </LinkContainer>
+  </Nav>
 );
 
 export default Header;
