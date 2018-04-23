07c48467bb669d689118b9e2f3ac251ff3550217
David Leigh <daleighan@gmail.com>
2018-04-23 02:58:00 -0700

    Some refactoring

--- src/components/app.js
+++ src/components/app.js
@@ -5,35 +5,11 @@ import {initialFetch, decreaseProp} from '../../store/actions/projectActions';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
+const App = () => (
+  <div>
+    <Header />
+    <Routes />
+  </div>
+);
 
-  componentDidMount() {
-    const {initialFetch} = this.props;
-    initialFetch();
-  }
-  render = () => {
-    return (
-      <div>
-        <Header />
-        <Routes />
-      </div>
-    );
-  };
-}
-
-const mapStateToProps = ({projectReducer}) => {
-  return {
-    projects: projectReducer.projects,
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    initialFetch: bindActionCreators(initialFetch, dispatch),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- store/actions/projectActions.js
+++ store/actions/projectActions.js
@@ -4,7 +4,6 @@ export const initialFetch = () => dispatch => {
   axios
     .get('https://bwuz5to7ch.execute-api.us-east-2.amazonaws.com/dev/projects')
     .then(response => {
-      console.log(response.data);
       return dispatch({type: 'FETCH_PROJECTS', payload: response.data.Items});
     })
     .catch(err => console.log(err));
