830b9ad6fa45883deea090b527d0635e790af22a
David Leigh <daleighan@gmail.com>
2018-04-24 14:10:16 -0700

    The icons are now positioned correctly for both mobile and desktop views

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,18 @@ import '../../scss/footerStyles.scss';
 
 const Footer = () => (
   <div className="footer">
-    <img src="email.png" />
-    <img src="cellphone-android.png" />
-    <img src="github-box.png" />
-    <img src="linkedin-box.png" />
+    <span className="icon-holder">
+      <img className="footer-icon" src="email.png" />
+    </span>
+    <span className="icon-holder">
+      <img className="footer-icon" src="cellphone-android.png" />
+    </span>
+    <span className="icon-holder">
+      <img className="footer-icon" src="github-box.png" />
+    </span>
+    <span className="icon-holder">
+      <img className="footer-icon" src="linkedin-box.png" />
+    </span>
   </div>
 );
 
