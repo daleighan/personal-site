705c23a3f7dde0a87bcd4ed57c08be445a8e5643
David Leigh <daleighan@gmail.com>
2018-04-24 16:28:26 -0700

    Put tooltips on the footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,40 @@
 import React from 'react';
+import {UncontrolledTooltip} from 'reactstrap';
 import '../../scss/footerStyles.scss';
 
 const Footer = () => (
   <div className="footer">
     <span className="icon-holder">
-      <img className="footer-icon" src="email.png" />
+      <a href="#" id="emailTooltip">
+        <img className="footer-icon" src="email.png" />
+      </a>
+      <UncontrolledTooltip autohide={false} placement="top" target="emailTooltip">
+        Hello world!
+      </UncontrolledTooltip>
     </span>
     <span className="icon-holder">
-      <img className="footer-icon" src="cellphone-android.png" />
+      <a href="#" id="phoneTooltip">
+        <img className="footer-icon" src="cellphone-android.png" />
+      </a>
+      <UncontrolledTooltip autohide={false} placement="top" target="phoneTooltip">
+        Hello world!
+      </UncontrolledTooltip>
     </span>
     <span className="icon-holder">
-      <img className="footer-icon" src="github-box.png" />
+      <a href="#" id="githubTooltip">
+        <img className="footer-icon" src="github-box.png" />
+      </a>
+      <UncontrolledTooltip autohide={false} placement="top" target="githubTooltip">
+        Hello world!
+      </UncontrolledTooltip>
     </span>
     <span className="icon-holder">
-      <img className="footer-icon" src="linkedin-box.png" />
+      <a href="#" id="linkedinTooltip">
+        <img className="footer-icon" src="linkedin-box.png" />
+      </a>
+      <UncontrolledTooltip autohide={false} placement="top" target="linkedinTooltip">
+        Hello world!
+      </UncontrolledTooltip>
     </span>
   </div>
 );
