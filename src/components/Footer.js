6d8e42f5f9fc532bba6bf112152337f33fb72eaa
David Leigh <daleighan@gmail.com>
2018-04-25 01:26:05 -0700

    Changed the order of the footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,19 +5,19 @@ import '../../scss/footerStyles.scss';
 const Footer = () => (
   <div className="footer">
     <span className="icon-holder">
-      <a href="mailto:daleighan@gmail.com" id="emailTooltip">
-        <img className="footer-icon" src="email.png" />
-      </a>
-      <UncontrolledTooltip autohide={false} placement="top" target="emailTooltip">
-        <a href="mailto:daleighan@gmail.com" className="no-underline">Email Me</a>
-      </UncontrolledTooltip>
-    </span>
-    <span className="icon-holder">
       <a href="tel:1-202-550-0872" id="phoneTooltip">
         <img className="footer-icon" src="cellphone-android.png" />
       </a>
       <UncontrolledTooltip autohide={false} placement="top" target="phoneTooltip">
         <a href="tel:1-202-550-0872" className="no-underline">Call Me</a> 
+      </UncontrolledTooltip>
+    </span>
+    <span className="icon-holder">
+      <a href="mailto:daleighan@gmail.com" id="emailTooltip">
+        <img className="footer-icon" src="email.png" />
+      </a>
+      <UncontrolledTooltip autohide={false} placement="top" target="emailTooltip">
+        <a href="mailto:daleighan@gmail.com" className="no-underline">Email Me</a>
       </UncontrolledTooltip>
     </span>
     <span className="icon-holder">
