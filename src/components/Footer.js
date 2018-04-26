9101a67eb5bb0635242001ea235ee7ecc41577c3
David Leigh <daleighan@gmail.com>
2018-04-25 19:05:09 -0700

    Refactored the footer items

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,40 +2,47 @@ import React from 'react';
 import {UncontrolledTooltip} from 'reactstrap';
 import '../../scss/footerStyles.scss';
 
+const footerItems = [
+  {
+    ref: 'tel:1-202-550-0872',
+    id: 'phoneTooltip',
+    src: 'cellphone-android.png',
+    text: 'Call Me',
+  },
+  {
+    ref: 'mailto:daleighan@gmail.com',
+    id: 'emailTooltip',
+    src: 'email.png',
+    text: 'Email Me',
+  },
+  {
+    ref: 'https://github.com/daleighan',
+    id: 'githubTooltip',
+    src: 'github-box.png',
+    text: 'My GitHub',
+  },
+  {
+    ref: 'https://www.linkedin.com/in/alexander-leigh',
+    id: 'linkedinTooltip',
+    src: 'linkedin-box.png',
+    text: 'My LinkedIn',
+  },
+];
+
 const Footer = () => (
   <div className="footer">
-    <span className="icon-holder">
-      <a href="tel:1-202-550-0872" id="phoneTooltip">
-        <img className="footer-icon" src="cellphone-android.png" />
-      </a>
-      <UncontrolledTooltip autohide={false} placement="top" target="phoneTooltip">
-        <a href="tel:1-202-550-0872" className="no-underline">Call Me</a> 
-      </UncontrolledTooltip>
-    </span>
-    <span className="icon-holder">
-      <a href="mailto:daleighan@gmail.com" id="emailTooltip">
-        <img className="footer-icon" src="email.png" />
-      </a>
-      <UncontrolledTooltip autohide={false} placement="top" target="emailTooltip">
-        <a href="mailto:daleighan@gmail.com" className="no-underline">Email Me</a>
-      </UncontrolledTooltip>
-    </span>
-    <span className="icon-holder">
-      <a href="http://github.com/daleighan" id="githubTooltip">
-        <img className="footer-icon" src="github-box.png" />
-      </a>
-      <UncontrolledTooltip autohide={false} placement="top" target="githubTooltip">
-        <a href="https://github.com/daleighan" className="no-underline">My GitHub</a>
-      </UncontrolledTooltip>
-    </span>
-    <span className="icon-holder">
-      <a href="https://www.linkedin.com/in/alexander-leigh" id="linkedinTooltip">
-        <img className="footer-icon" src="linkedin-box.png" />
-      </a>
-      <UncontrolledTooltip autohide={false} placement="top" target="linkedinTooltip">
-        <a href="https://www.linkedin.com/in/alexander-leigh" className="no-underline">My LinkedIn</a>
-      </UncontrolledTooltip>
-    </span>
+    {footerItems.map(item => (
+      <span className="icon-holder">
+        <a href={item.ref} id={item.id}>
+          <img className="footer-icon" src={item.src} />
+        </a>
+        <UncontrolledTooltip autohide={true} placement="top" target={item.id}>
+          <a href={item.ref} className="no-underline">
+            {item.text}
+          </a>
+        </UncontrolledTooltip>
+      </span>
+    ))}
   </div>
 );
 
