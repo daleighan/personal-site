e3ac66812f33bb89cde6280e91bbf976eddf76e9
David Leigh <daleighan@gmail.com>
2018-04-27 03:41:31 -0700

    Added a scroll animation

--- src/components/ProjectsEntry.js
+++ src/components/ProjectsEntry.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import {Card, Button, CardTitle, CardImg, CardBody} from 'reactstrap';
 
+const scrollToTop = scrollDuration => {
+  const scrollStep = -window.scrollY / (scrollDuration / 15);
+  const scrollInterval = setInterval(() => {
+    if (window.scrollY != 0) {
+      window.scrollBy(0, scrollStep);
+    } else clearInterval(scrollInterval);
+  }, 15);
+};
+
 const ProjectsEntry = ({project, setProject, nav}) => {
   const {projectName, pictures, blurb} = project;
   return (
@@ -9,7 +18,10 @@ const ProjectsEntry = ({project, setProject, nav}) => {
       <CardImg width="100%" src={pictures[0].src} alt={projectName} />
       <CardBody>{blurb}</CardBody>
       <Button
-        onClick={() => setProject(project, nav)}
+        onClick={() => {
+          setProject(project, nav);
+          scrollToTop(200);
+        }}
         className="btn-projects"
         color="info"
         size="sm">
